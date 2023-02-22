e0b3ec58b9aef9fd8ae900c7c95551dbfac6dfee
Gabriel Bekier <gabrielbekier@gmail.com>
2023-02-22 18:42:31 +0100

    recipe - dropdown menu

--- recipe-app/recipe-app/src/assets/directive/dropdown.directive.ts
+++ recipe-app/recipe-app/src/assets/directive/dropdown.directive.ts
@@ -1,12 +1,29 @@
-import { Directive, HostListener } from '@angular/core';
+import { Element } from '@angular/compiler';
+import { AfterViewInit, Directive, ElementRef, HostBinding, HostListener, Renderer2, ViewChild } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective {
+export class DropdownDirective implements AfterViewInit{
 
-  // @HostListener('click')
+ parentTag: ElementRef;
 
-  constructor() { }
 
+  @HostListener('click') toggleOpen(){
+    this.parentTag.nativeElement.querySelector('.dropdown-menu').classList.toggle('show');
+  }
+  // @HostListener('document:click',['$event']) closeDropdown(event: Event){
+  //   if ((this.parentTag.nativeElement.querySelector('.dropdown-menu').classList.contains('show'))&& (this.elem.nativeElement.contains(event.target))){
+  //     this.parentTag.nativeElement.querySelector('.dropdown-menu').classList.remove('show')
+  //   } else{
+  //     return;
+  //   };
+  // }
+
+  constructor(private elem: ElementRef) {
+    this.parentTag = elem;
+   }
+
+   ngAfterViewInit(): void {
+    }
 }
